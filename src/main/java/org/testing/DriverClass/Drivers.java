9fc8d1385894ba05f86b5098250a191b0cc9a694
Akash Kesarwani <kesarwania017@gmail.com>
2026-03-16 18:48:34 +0530

    Update Drivers.java

--- src/main/java/org/testing/DriverClass/Drivers.java
+++ src/main/java/org/testing/DriverClass/Drivers.java
@@ -1,6 +1,7 @@
 package org.testing.DriverClass;
 
 import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.Collections;
