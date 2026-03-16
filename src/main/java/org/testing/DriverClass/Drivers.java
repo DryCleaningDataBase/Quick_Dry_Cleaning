41c1d72641b5229b11a1076b3d9292090b29a263
DryCleaningDataBase <kesarwania017@gmail.com>
2026-03-16 18:40:25 +0530

    Update Drivers.java

--- src/main/java/org/testing/DriverClass/Drivers.java
+++ src/main/java/org/testing/DriverClass/Drivers.java
@@ -1,16 +1,28 @@
-public static WebDriver openChromeBrowser() {
-    ChromeOptions options = new ChromeOptions();
-    
-    options.addArguments("--headless=new");
-    options.addArguments("--window-size=1920,1080");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--remote-allow-origins=*");
-    options.addArguments("--disable-gpu");
-    options.addArguments("--ignore-certificate-errors");
-    options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-    options.setExperimentalOption("useAutomationExtension", false);
-    options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
+package org.testing.DriverClass;
 
-    return new ChromeDriver(options);
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.Collections;
+
+public class Drivers {
+    
+    public static WebDriver openChromeBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        
+        options.addArguments("--headless=new"); 
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--ignore-certificate-errors");
+        
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
+        
+        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
+
+        return new ChromeDriver(options);
+    }
 }
