809aa299f95cf8f597f3fea6774b23175ce711ac
DryCleaningDataBase <kesarwania017@gmail.com>
2026-03-16 18:38:17 +0530

    Update Drivers.java

--- src/main/java/org/testing/DriverClass/Drivers.java
+++ src/main/java/org/testing/DriverClass/Drivers.java
@@ -1,28 +1,16 @@
-package org.testing.DriverClass;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import java.util.Collections; // Recommended for cleaner option management
-
-public class Drivers {
+public static WebDriver openChromeBrowser() {
+    ChromeOptions options = new ChromeOptions();
     
-    public static WebDriver openChromeBrowser() {
-        ChromeOptions options = new ChromeOptions();
-        
-        // Essential Headless & CI Flags
-        options.addArguments("--headless=new");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--ignore-certificate-errors");
-        
-        // This flag helps bypass "Bot Detection" which might be causing your redirect to fail
-        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-        options.setExperimentalOption("useAutomationExtension", false);
+    options.addArguments("--headless=new");
+    options.addArguments("--window-size=1920,1080");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--remote-allow-origins=*");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--ignore-certificate-errors");
+    options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+    options.setExperimentalOption("useAutomationExtension", false);
+    options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
 
-        return new ChromeDriver(options);
-    }
+    return new ChromeDriver(options);
 }
